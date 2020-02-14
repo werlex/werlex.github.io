5ae10bf88c018206a52848c72c5dae8036dca47d
Jokubas Kytra <jokubaskytra@gmail.com>
2020-02-14 18:28:37 +0200

    Updated spacex

---
+++ spacex/precache-manifest.eadf4e905962f4733f91121d39e48830.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a709340cf88a561d5efd7a30fa228093",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cf69b077868bcaae3102",
+    "url": "./static/css/main.9d6d61a5.chunk.css"
+  },
+  {
+    "revision": "27224aadc4968d1c912c",
+    "url": "./static/js/2.b587e908.chunk.js"
+  },
+  {
+    "revision": "cf69b077868bcaae3102",
+    "url": "./static/js/main.3376ac81.chunk.js"
+  },
+  {
+    "revision": "e6a02653d0cb5efe268d",
+    "url": "./static/js/runtime-main.240e6af1.js"
+  }
+]);
--- spacex/service-worker.js
+++ spacex/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.17bdee2dcdecf0f3cf68154ebfbd5e43.js"
+  "./precache-manifest.eadf4e905962f4733f91121d39e48830.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
